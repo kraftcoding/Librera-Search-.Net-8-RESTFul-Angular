a80dea64a6189a7c13eab8b4f31eb839a8bb0cd3
Kraftcoding <i.elg.alzaa@gmail.com>
2025-11-30 09:47:17 -0800

    Jwt bearer authentication is added

--- Librera-Search-WebAPI/Models/LibreraSearchApiContext.cs
+++ Librera-Search-WebAPI/Models/LibreraSearchApiContext.cs
@@ -16,22 +16,25 @@ public partial class LibreraSearchApiContext : DbContext
     }
 
     public virtual DbSet<Books> Books { get; set; }
+    public virtual DbSet<Login> Login { get; set; }
 
-    #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer("Server=DEV01-WIN-SERV-2022;Database=LibreraSearch;Trusted_Connection=True; Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Books>(entity =>
+        //modelBuilder.Entity<Books>(entity =>
+        //{
+        //    entity.HasIndex(e => e.id, "id");
+        //    //entity.Property(e => e.title).HasColumnName("title");
+        //    //entity.HasOne(d => d.User).WithMany(p => p.Books).HasForeignKey(d => d.UserId);
+        //});
+
+        modelBuilder.Entity<Login>(entity =>
         {
             entity.HasIndex(e => e.id, "id");
-
-            //entity.Property(e => e.title).HasColumnName("title");
-
-            //entity.HasOne(d => d.User).WithMany(p => p.Books).HasForeignKey(d => d.UserId);
-
         });
 
         OnModelCreatingPartial(modelBuilder);
